18be4509e9effb8c2ff58784900d53e363a34e17
marcuspimenta <mvinicius.pimenta@gmail.com>
2012-11-02 20:20:53 -0300

    Chat Bluetooth Android

---
+++ Bluetooth/src/com/bluetooth/manager/BluetoothClient.java
@@ -0,0 +1,33 @@
+package com.bluetooth.manager;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+/**
+ * 
+ * @author Marcus Pimenta
+ * @email mvinicius.pimenta@gmail.com
+ * 01/11/2012 13:05:05 
+ */
+public class BluetoothClient{
+	 
+	 private final UUID MEU_UUID_PC = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+	
+	 public BluetoothSocket conectedBluetooth(BluetoothDevice device) {
+		 BluetoothSocket socket = null;
+		 
+		 try {
+			 socket = device.createRfcommSocketToServiceRecord(MEU_UUID_PC);
+			 socket.connect();
+			 
+		 } catch (IOException e) { 
+			 e.printStackTrace();
+		 }
+		 
+		 return socket;
+	 }
+	 
+ }
---
+++ Bluetooth/src/com/bluetooth/manager/BluetoothComunication.java
@@ -0,0 +1,99 @@
+package com.bluetooth.manager;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+
+/**
+ * 
+ * @author Marcus Pimenta
+ * @email mvinicius.pimenta@gmail.com
+ * 01/11/2012 13:06:15 
+ */
+public class BluetoothComunication extends Thread {
+	 
+	private int whatMsgBT;
+	private int whatMsgNotice;
+	
+	private Handler handler;
+	
+	private BluetoothSocket socket;
+	private DataInputStream dataInputStream = null;
+	private DataOutputStream dataOutputStream = null;
+	
+	public BluetoothComunication(Handler handler, int whatMsgBT, int whatMsgNotice){
+		this.handler = handler;
+		this.whatMsgBT = whatMsgBT;
+		this.whatMsgNotice = whatMsgNotice;
+	}
+	
+	public void openComunication(BluetoothSocket socket){
+		this.socket = socket;
+		start();
+	}
+	 
+	@Override
+	public void run() {
+		 super.run();
+		
+		 String nameBluetooth;
+		
+		 try {
+			 nameBluetooth = socket.getRemoteDevice().getName();
+			 dataInputStream = new DataInputStream(socket.getInputStream());
+			 dataOutputStream = new DataOutputStream(socket.getOutputStream());
+			
+			 sendHandler(whatMsgNotice, "Conexão realizada com sucesso");
+			 
+			 while (true) {
+				 if(dataInputStream.available() > 0){
+					 byte[] msg = new byte[10];
+					 dataInputStream.read(msg, 0, msg.length);
+					 
+					 sendHandler(whatMsgBT, nameBluetooth + ": " + new String(msg));
+				 }
+			 }
+		 } catch (IOException e) {
+			 e.printStackTrace(); 
+			 
+			 dataInputStream = null;
+			 dataOutputStream = null;
+			 
+			 sendHandler(whatMsgNotice, "Conexão perdida");
+		 }
+	}
+	
+	public void sendMessageByBluetooth(String msg){
+		try {
+			if(dataOutputStream != null){
+				dataOutputStream.write(msg.getBytes());
+			}else{
+				sendHandler(whatMsgNotice, "Sem conexão");
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace(); 
+			 
+			sendHandler(whatMsgNotice, "Falha no envio da mensagem");
+		}
+	}
+	
+	public void sendHandler(int what, Object object){
+		handler.obtainMessage(what, object).sendToTarget();
+	}
+           
+	 public void stopComunication(){ 
+		try {
+			if(dataInputStream != null && dataOutputStream != null){
+				dataInputStream.close();
+				dataOutputStream.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	 }
+	 
+ }
